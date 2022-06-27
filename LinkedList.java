710c81cf6c2b00dafe7d849da2e5d4a5849aa298
HediMuhamad <hedymuhamad461@gmail.com>
2022-06-27 04:00:41 +0300

    Singly-LinkedList completed.

--- LinkedList.java
+++ LinkedList.java
@@ -25,7 +25,22 @@ public class LinkedList<T>{
 	 * @return The node in the specified index.
 	 */
 	private Node<T> getAsNode(int from) {
-		return null;
+		
+		if(from>=size || 0>from) {
+			return null; 
+		}
+		
+		Node<T> node = firstNode;
+		
+		if(from==size-1) {
+			node = lastNode;
+		}
+		
+		for(int i=0;i<from;i++) {
+			node = node.getNextNode();
+		}
+		
+		return node;
 	}
 	
 	/** Gets the node's value in the specified index.
@@ -33,35 +48,39 @@ public class LinkedList<T>{
 	 * @return The node's value in the specified index.
 	 */
 	public T get(int from) {
-		return null;
+		return getAsNode(from).getValue();
 	}
 	
 	/** Gets the first node's value.
 	 * @return The first node's value in the LinkedList.
 	 * */
 	public T getFirst() {
-		return null;
+		return get(0);
 	}
 	
 	/** Gets the last node's value.
 	 * @return The last node's value in the LinkedList.
 	 * */
 	public T getLast() {
-		return null;
+		return get(size-1);
 	}
 	
 	/** Gets the first node's value and removes the node.
 	 * @return The first node's value in the LinkedList.
 	 */
 	public T popFirst() {
-		return null;
+		T firstNodeValue = firstNode.getValue();
+		firstNode = firstNode.getNextNode();
+		return firstNodeValue;
 	}
 	
 	/** Gets the last node's value and removes the node.
 	 * @return The the last node's value in the LinkedList.
 	 */
 	public T popLast() {
-		return null;
+		T lastNodeValue = lastNode.getValue();
+		lastNode = getAsNode(size-2);
+		return lastNodeValue;
 	}
 	
 	/** Sets the specified value into specified index.
@@ -70,6 +89,13 @@ public class LinkedList<T>{
 	 * @return A boolean variable type, True if succeed, False if failed.
 	 */
 	public boolean set(int in, T value) {
+		Node<T> node = getAsNode(in);
+		
+		if(node!=null) {
+			node.setValue(value);
+			return true;
+		}
+		
 		return false;
 	}
 	
@@ -78,7 +104,15 @@ public class LinkedList<T>{
 	 * @param values Specified values.
 	 */
 	public boolean setAll(int in, T[] values) {
-		return false;
+		if(0>in || size-in<values.length) {
+			return false;
+		}
+		
+		for(int i=0;i<values.length;i++) {
+			set(i+in, values[i]);
+		}
+		
+		return true;
 	}
 	
 	/** Sets the specified values into specified indexes.
@@ -86,14 +120,40 @@ public class LinkedList<T>{
 	 * @param values Specified values.
 	 */
 	public boolean setAll(int[] in, T[] values) {
-		return false;
+		if(values.length!=in.length || size-values.length<values.length) {
+			return false;
+		}
+
+		for(int i=0;i<in.length;i++) {
+			if(0>in[i] || in[i]>=size) {
+				return false;
+			}
+		}
+		
+		for(int i=0;i<values.length;i++) {
+			set(in[i], values[i]);
+		}
+		
+		return true;
 	}
 
 	/** Adds a single node with specified value to the end of the LinkedList.
 	 * @param value Specified value.
 	 */
 	public boolean add(T value) {
-		return false;
+		Node<T> newNode = new Node<T>(value);
+		if(size==0) {
+			firstNode = newNode;
+		}else if(size==1) {
+			firstNode.setNextNode(newNode);
+		}else {
+			lastNode.setNextNode(newNode);			
+		}
+		
+		lastNode = newNode;
+		size++;
+		
+		return true;
 	}
 	
 	/** Adds a single node from the specified index with specified value.
@@ -101,7 +161,45 @@ public class LinkedList<T>{
 	 * @param value Specified value.
 	 */
 	public boolean add(int from, T value) {
-		return false;
+		
+		if(0>from) {
+			return false;
+		}
+
+		if(from>size) {
+			for(int i=size;i<size+(from-size);i++) {
+				add(null);
+			}
+			add(value);
+			return true;
+		}
+		
+
+		Node<T> newNode= new Node<T>(value),
+				previousNode=null,
+				nextNode=null;
+		
+		if(from==0) {
+			newNode.setNextNode(firstNode);
+			firstNode = newNode;
+			size++;
+		}else if(from==size){
+			add(value);
+		}else {
+			previousNode = getAsNode(from-1);
+			nextNode = previousNode.getNextNode();
+			
+			newNode.setNextNode(nextNode);
+			previousNode.setNextNode(newNode);
+
+			if(from==size) {
+				lastNode=newNode;
+			}
+			size++;
+		}
+		
+		
+		return true;
 	}
 	
 	/** Adds multiple nodes from the specified indexes with specified value.
@@ -109,7 +207,15 @@ public class LinkedList<T>{
 	 * @param values Specified values.
 	 */
 	public boolean addAll(int from, T[] values) {
-		return false;
+		if(0>from) {
+			return false;
+		}
+		
+		for(int i=0;i<values.length;i++) {
+			add(i+from, values[i]);
+		}
+		
+		return true;
 	}
 	
 	/** Adds multiple nodes to specified indexes with specified values.
@@ -117,21 +223,38 @@ public class LinkedList<T>{
 	 * @param values Specified values.
 	 */
 	public boolean addAll(int[] from, T[] values) {
-		return false;
+		
+		if(from.length!=values.length) {
+			return false;
+		}
+		
+		for(int i=0;i<from.length;i++) {
+			if(0>from[i]) {
+				return false;
+			}
+		}
+		
+		for(int i=0;i<values.length;i++) {
+			add(from[i], values[i]);
+		}
+		
+		return true;
 	}
 
 	/** Adds a single node at first with specified value.
 	 * @param value Specified value.
 	 */
 	public boolean pushFirst(T value) {
-		return false;
+		add(0, value);
+		return true;
 	}
 
 	/** Adds a single node to the end with specified value.
 	 * @param value Specified value.
 	 */
 	public boolean pushLast(T value) {
-		return false;
+		add(value);
+		return true;
 	}
 	
 	/** Removes a single node in the array if it's value is equal to specified value.
@@ -139,7 +262,7 @@ public class LinkedList<T>{
 	 * @return The removed index or (-1) if failed.
 	 * */
 	public int remove(T value) {
-		return -1;
+		return remove(0, value);
 	}
 	
 	/** Removes a single node in the array which placed in or after index {@code from} and it's value is equal to specified value.
@@ -148,7 +271,7 @@ public class LinkedList<T>{
 	 * @return The removed index or (-1) if failed.
 	 * */	
 	public int remove(int from, T value) {
-		return -1;
+		return delete(find(from, value))==true ? 1 : -1 ;
 	}
 	
 	/** Removes all nodes in the array which their values is equal to specified values.
@@ -156,7 +279,7 @@ public class LinkedList<T>{
 	 * @return The removed indexes or (null) if failed.
 	 * */	
 	public int[] removeAll(T[] values) {
-		return null;
+		return removeAll(0, values);
 	}
 
 	/** Removes all nodes in the array which they placed in or after index {@code from} and their values are equal to specified values.
@@ -165,7 +288,11 @@ public class LinkedList<T>{
 	 * @return The removed indexes or (null) if failed.
 	 * */	
 	public int[] removeAll(int from, T[] values) {
-		return null;
+		int[] result = new int[values.length];
+		for(int i=0; i<result.length; i++) {
+			result[i] = remove(from, values[i]);
+		}
+		return result;
 	}
 
 
@@ -175,29 +302,60 @@ public class LinkedList<T>{
 	 * @return The removed indexes or (null) if failed.
 	 * */	
 	public int[] removeAll(int[] from, T[] values) {
-		return null;
+		if(from.length != values.length) {
+			return null;
+		}
+		
+		int[] result = new int[values.length];
+		for(int i=0; i<result.length; i++) {
+			result[i] = remove(from[i], values[i]);
+		}
+		return result;
 	}
 
 	/**	Deletes the node at specified index.
 	 * @param index The specified index
 	 * */
 	public boolean delete(int index) {
-		return false;
+		if(0>index || index>=size) {
+			return false;
+		}
+		
+		Node<T> previousNode = getAsNode(index-1);
+		Node<T>	nextNode= previousNode.getNextNode().getNextNode();
+		
+		previousNode.setNextNode(nextNode);
+		
+		size--;
+		
+		return true;
 	}
 
 	/**	Deletes all nodes at specified indexes.
 	 * @param indexes The specified indexes.
 	 * */
 	public boolean deleteAll(int[] indexes) {
-		return false;
+		return deleteAll(indexes.length, indexes);
 	}	
 
 	/**	Deletes all elements at specified indexes.
 	 * @param length Specified length to detect a limit to the deleting process.
 	 * @param indexes The specified indexes.
 	 * */
-	public boolean deleteAll(int length, int[] indexes) {		
-		return false;
+	public boolean deleteAll(int length, int[] indexes) {
+
+		for(int i=0;i<length;i++) {
+			if(0>indexes[i] || indexes[i]>=size) {
+				return false;
+			}
+		}
+		
+		for(int i=0;i<length;i++) {
+			delete(indexes[i]);
+		}
+		
+		return true;
+		
 	}
 	
 	/**	Searches for a specific value using Linear Search.
@@ -205,7 +363,7 @@ public class LinkedList<T>{
 	 * @return The first index which the value found in, if didn't it will return -1.
 	 * */
 	public int find(T value) {
-		return -1;
+		return find(0, value);
 	}
 
 	/**	Searches for a specific value using Linear Search, it starts at specified index.
@@ -214,6 +372,24 @@ public class LinkedList<T>{
 	 * @return The first index which the value found in, if the index is equal or greater to {@code from}, if didn't it will return -1.
 	 * */
 	public int find(int from, T value) {
+
+		if(0>from || from>=size) {
+			return -1;
+		}
+		
+		if(from==size-1) {
+			return lastNode.compareTo(value)==0 ? size-1 : -1;
+		}
+		
+		Node<T> currentNode = firstNode;
+		
+		for(int i=from;i<size;i++) {
+			if(currentNode.compareTo(value)==0) {
+				return i;
+			}
+			currentNode = currentNode.getNextNode();
+		}
+		
 		return -1;
 	}
 	
@@ -222,7 +398,8 @@ public class LinkedList<T>{
 	 * @return The last index which the value found in, if didn't it will return -1.
 	 * */
 	public int findInLast(T value) {
-		return -1;
+		return findInLast(size-1, value);
+		
 	}
 
 	/**	Searches for a specific value using  Linear Search, it starts at specified index.
@@ -231,35 +408,93 @@ public class LinkedList<T>{
 	 * @return The first index which the value found in, if the index is equal or minor to {@code from}, if didn't it will return -1.
 	 * */
 	public int findInLast(int from, T value) {
-		return -1;
+
+		if(0>from || from>=size) {
+			return -1;
+		}
+		
+		if(from==0) {
+			return firstNode.compareTo(value)==0 ? 0 : -1;
+		}
+		
+		int foundIndex = -1;
+
+		Node<T> currentNode = firstNode;
+		
+		for(int i=0;i<=from;i++) {
+			if(currentNode.compareTo(value)==0) {
+				foundIndex = i;
+			}
+			currentNode = currentNode.getNextNode();
+		}
+		
+		return foundIndex;
 	}
 	
 	/**	Returns the size of the LinkedList.
 	 * @return An integer value which represent the size of the LinkedList.
 	 * */
 	public int size() {
-		return -1;
+		return size;
 	}
 	
 	/** Checks for the emptiness of the LinkedList.
 	 * @return A boolean value, true if empty, and false if not.
 	 * */
 	public boolean isEmpty() {
-		return false;
+		return size==0;
 	}
 	
 	/**	Reverses the LinkedList nodes order.
 	 * @throws CloneNotSupportedException 
 	 * */
 	public void reverse() throws CloneNotSupportedException {
+		
+		if(size<=1) {
+			return;
+		}
+		
+		Node<T> holderNode = firstNode.getNextNode();
+		Node<T> holdedNode = firstNode;
+		firstNode.setNextNode(null);
+		
+		for(int i=0;i<size-2;i++) {
+			Node<T> tempNode = holderNode.getNextNode();
+			holderNode.setNextNode(holdedNode);
+			holdedNode=holderNode;
+			holderNode=tempNode;
+		}
+		holderNode.setNextNode(holdedNode);
+
+		Node<T> tempNode = lastNode;
+	 	lastNode = firstNode;
+	 	firstNode = tempNode;
 	}
+
 	
 	/**	Returns the values of the nodes in a readable text.
 	 * @return A string value which contain all nodes value's.
 	 * */
 	@Override
 	public String toString() {
-		return super.toString();
+		super.toString();
+
+		if(size==0) {
+			return "[]";
+		}
+		
+		StringBuilder str = new StringBuilder();
+		str.append("[");
+		
+		Node<T> node = firstNode;
+		
+		for(int i=0;i<size-1;i++) {
+			str.append(node.getValue() + ", ");
+			node = node.getNextNode();
+		}
+		str.append(lastNode.getValue() + "]");
+		
+		return str.toString();
 	}
 	
 	
@@ -282,31 +517,34 @@ class Node<T> implements Cloneable, Comparable<T>{
 	 * @param value The Node value.
 	 * */
 	Node(T value) {
+		this.value = value;
 	}
 	
 	/**	Returns the node value
 	 * @return Node value
 	 * */
 	public T getValue() {
-		return null;
+		return value;
 	}
 	
 	/** Sets the node value.
 	 * @param value The value which user want to set.
 	 * */
 	public void setValue(T value) {
+		this.value = value;
 	}
 	
 	/** Returns the next node of the current node.
 	 * @return The next node of the current node.
 	 * */
 	public Node<T> getNextNode() {
-		return null;
+		return nextNode;
 	}
 
 	/** Sets the next node of the current node.
 	 * */
 	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -317,7 +555,12 @@ class Node<T> implements Cloneable, Comparable<T>{
 
 	@Override
 	public int compareTo(T o) {
-		return -1;
+		
+		if(this.getValue()==null) {
+			return o==null ? 0 : -1;
+		}
+		
+		return this.getValue().equals(o) ? 0 : -1;
 	}
 	
 }
